5aa8e5369b5a79b84ffa8bc2b7011f39fa8ee864
Gleb Mazhara <gensores@mail.ru>
2022-12-28 12:02:03 +0300

    Add tasks

---
+++ task 41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите числа");
+int[] array =Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for(int i=0; i<array.Length; i++)
+{
+    if(array[i]<0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Колличество элементов меньше нуля: {count}");
---
+++ task 43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+double x= (k2-k1)/(b1-b2);
+double y= k1*x+b1;
+Console.WriteLine($"Координаты пересечения[{x},{y}]");
